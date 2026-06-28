4f7c6a39a782c93c14278fcb877a0340cfa590ed
Mohammad Sabeera <mujibamohammad058@gmail.com>
2026-06-28 16:50:45 +0530

    Added Day 2 Python conditional statements

---
+++ day2.py
@@ -0,0 +1,25 @@
+cgpa=8.5
+print("SAP Ready")
+if cgpa>=7.5:
+    print("keep working hard")
+else:
+    print("you need to work hard")
+
+# german salary checker
+salary = int(input("Enter your annual salary in Euros (€): "))
+
+if salary >= 55000:
+    print("EU Blue Card eligible!")
+elif salary >= 45000:
+    print("Borderline")
+else:
+    print("Need more experience")
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "sabeera" and password == "sap2028":
+    print("Welcome to SAP Portal!")
+else:
+    print("Wrong credentials!")
+
+   
